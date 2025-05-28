a34987ecb9325494c79c66791b67b16ba542ad89
Gustavo Feriani <guuh.feriani@gmail.com>
2025-05-28 17:00:38 -0300

    update firebase

--- forms/appointment.php
+++ forms/appointment.php
@@ -1,7 +1,6 @@
 <?php
-
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'guuh.feriani@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
